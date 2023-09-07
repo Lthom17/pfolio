58b340f0cb28e69a287c3e791d08f1c10802241e
Lauren Thom <laurenthom17@gmail.com>
2023-09-07 15:46:33 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './components/Home'
+import About from './components/About'
 const App = () => {
 
   return (
     <div className='App'>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={ <Home /> } />
+          <Route path='/' Component={<Home />} />
+          <Route path='about' Component={<About/>} />
         </Routes>
       </BrowserRouter>
       </div>
