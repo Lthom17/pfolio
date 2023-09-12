fcfb2647c5b1e0225ae66e36a47b5099a4533763
Lauren Thom <laurenthom17@gmail.com>
2023-09-12 09:43:31 -0500

    reddo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import Home from './components/Home'
+import Home from './components/Home';
 import About from './components/About'
 const App = () => {
 
